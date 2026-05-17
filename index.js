ab640fb8dc979efc6a2fdc561afac5ed83aa52e5
Asikur Rahaman <mdasikur5893@gmail.com>
2026-05-17 23:48:08 +0600

    favoritesReviews delete done in server site testing in thurnder client

--- index.js
+++ index.js
@@ -130,7 +130,11 @@ async function run() {
         //?favoritesReviews delete;
         app.delete('/favoritesReviewsColl/:id',async (req,res)=>{
             const id = req.params.id;
-            console.log(id);
+            const query = {_id:new ObjectId(id)};
+            const result = await allFevoritesReviews.deleteOne(query);
+            res.send(result);
+            // console.log(id);
+
         })
         //? creatNewFood post db data;
         app.post('/creatNewFood', verifyFireBaseToken2, async (req, res) => {
